910d537d658ef3d3f6fa9188e33a73b46ceeafe3
Guilherme Decampo <guilherme.decampo@gmail.com>
2016-08-10 13:14:51 -0300

    Add sidebar logic and buttons

--- app/imports/client/views/search/sideBar/sideBar.js
+++ app/imports/client/views/search/sideBar/sideBar.js
@@ -1,18 +1,52 @@
 import { Template } from 'meteor/templating'
+import { FlowRouter } from 'meteor/kadira:flow-router'
+import { Modal } from '/imports/both/modalApi'
+
+import _ from 'lodash'
+
+Template.sideBar.events({
+  'keyup .company-input'(event, template) {
+    const company = event.currentTarget.value
+
+    FlowRouter.setParams({company})
+  },
+  'click .js-new-competitor'() {
+    if (!Meteor.userId()) {
+      Modal.open('accountsModal', {
+        subTemplate: 'signIn',
+        state: 'signIn',
+      })
+
+      return
+    }
+
+    Modal.open('addNewCompanyModal')
+  },
+  'click .js-become-competitor'() {
+    Modal.open('addValidatorModal')
+  },
+})
 
 Template.sideBar.helpers({
-  item() {
-    return [
-      {
-        text: 'Filter',
-        items: [
-          {text: 'by continent', icon: 'fa-globe', id:'filter'},
-          {text: 'by country', icon: 'fa-globe', id:'filter'},
-          {text: 'by city', icon: 'fa-globe', id:'filter'},
-          {text: 'by village', icon: 'fa-globe', id:'filter'},
-          {text: 'Geolocate me!', icon: 'fa-globe', id:'filter'},
-        ],
-      }
-    ]
-  }
+  company: () => FlowRouter.getParam('company'),
+  showBecomeValidator: () =>
+    Meteor.user() && !_.get(Meteor.user(), 'profile.validator'),
+})
+
+Template.sideBar.onCreated(function() {
+  const debouncedSearch = _.debounce(() => {
+    const company = FlowRouter.getParam('company')
+    this.data.loading.set(true)
+
+    Meteor.callPromise('competitors.get', company)
+    .then(list => this.data.listCompetitors.set(list))
+    .catch(error => window.alert(error))
+    .done(() => this.data.loading.set(false))
+
+  }, 300)
+
+  this.autorun(() => {
+    FlowRouter.watchPathChange()
+    debouncedSearch()
+  })
 })
