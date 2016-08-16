e4b23829bcc456a6e1accac7bbee98b45016190c
Guilherme Decampo <guilherme.decampo@gmail.com>
2016-08-16 14:07:46 -0300

    Update competitors method

--- app/imports/both/methods/competitors.js
+++ app/imports/both/methods/competitors.js
@@ -1,11 +1,18 @@
 import { Meteor } from 'meteor/meteor'
+import Companies from '/imports/both/collections/companies'
 
 Meteor.methods({
   'competitors.get'(name) {
-    const company = Companies.findOne({name})
+    const company = Companies.findOne({name: { $regex: RegExp(name, 'i')}})
 
     if (!company) return []
 
-    return Companies.find({categories: {$in: company.categories}}).fetch()
+    const { _id, categories } = company
+
+    return Companies.find({
+      _id: {$ne: _id},
+      categories: {$in: categories},
+      isValid: true,
+    }).fetch()
   }
 })
