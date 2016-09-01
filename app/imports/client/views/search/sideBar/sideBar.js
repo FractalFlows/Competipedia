b6d20d189c91687e9144c52f30b82c2afdff204a
Guilherme Decampo <guilherme.decampo@gmail.com>
2016-09-01 09:48:11 -0300

    Update events and usage of is not logged in

--- app/imports/client/views/search/sideBar/sideBar.js
+++ app/imports/client/views/search/sideBar/sideBar.js
@@ -1,8 +1,8 @@
 import { Template } from 'meteor/templating'
 import { FlowRouter } from 'meteor/kadira:flow-router'
 import { Modal } from '/imports/both/modalApi'
-import { Roles } from 'meteor/alanning:roles'
 import { Meteor } from 'meteor/meteor'
+import isNotLoggedIn from '/imports/client/lib/isNotLoggedIn'
 
 import _ from 'lodash'
 import denodeify from 'denodeify'
@@ -13,27 +13,21 @@ Template.sideBar.events({
 
     FlowRouter.setParams({company})
   },
-  'click .js-new-company'() {
-    if (!Meteor.userId()) {
-      Modal.open('accountsModal', {
-        subTemplate: 'signIn',
-        state: 'signIn',
-      })
+  'click .js-new-competitor'() {
+    if (isNotLoggedIn()) return
 
-      return
-    }
-
-    Modal.open('newCompanyModal', {
-      name: FlowRouter.getParam('company'),
-    })
+    Modal.open('newCompetitorModal')
   },
   'click .js-request-competitor'() {
+    if (isNotLoggedIn()) return
 
     Modal.open('requestCompetitorModal', {
       name: FlowRouter.getParam('company'),
     })
   },
   'click .js-become-validator'() {
+    if (isNotLoggedIn()) return
+
     Modal.open('addValidatorModal')
   },
 })
@@ -54,7 +48,6 @@ Template.sideBar.onCreated(function() {
     .then(list => this.data.listCompetitors.set(list))
     .catch(error => window.alert(error))
     .done(() => this.data.loading.set(false))
-
   }, 300)
 
   this.autorun(() => {
